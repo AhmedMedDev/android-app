340ae900454de01b310dbf5bbc4e2a66bded981e
unknown <dedoa82.as@gmail.com>
2022-04-10 07:28:54 +0200

    web view settings

--- app/src/main/java/com/example/android_mid/MainActivity.java
+++ app/src/main/java/com/example/android_mid/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView1 = (WebView) findViewById(R.id.webview1);
+
+        webView1.setWebViewClient(new WebViewClient());
+
+        WebSettings webSetting = webView1.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+
+        webSetting.setAllowContentAccess(true);
+        webSetting.setLoadWithOverviewMode(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setLoadsImagesAutomatically(true);
+        webSetting.setAppCacheEnabled(true);
+
+//        webSetting.setMixedContentMode (WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+//
+//        checkWebPage cwp= new checkwebPage();
+//        cwp.execute();
+//        txv1 = (TextView) findViewById (R.id.txv1);
+//        else if(view.getId()==R.id.RSugg) {
+//            webView1.loadUrl("https://forms.office.com/r/HfejjtfynN");
+//        }
     }
 
     public void changeWebPage(View view) {
