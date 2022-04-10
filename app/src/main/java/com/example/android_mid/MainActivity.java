65031450d3ef1b76411997a65c9ca2e58d410844
unknown <dedoa82.as@gmail.com>
2022-04-10 07:10:25 +0200

    Create : change web page fun

--- app/src/main/java/com/example/android_mid/MainActivity.java
+++ app/src/main/java/com/example/android_mid/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.android_mid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView1 = (WebView) findViewById(R.id.webview1);
     }
-}
+
+    public void changeWebPage(View view) {
+        if (view.getId() == R.id.REvents) {
+            webView1.loadUrl("https://science.asu.edu.eg/ar/events");
+        } else if (view.getId() == R.id.RNews) {
+            webView1.loadUrl("https://science.asu.edu.cg/ar/news");
+        }
+    }
+
+    }
