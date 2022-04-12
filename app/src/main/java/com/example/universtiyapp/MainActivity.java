771ce2e935d0894c780bd63110f9e303941ef41e
unknown <dedoa82.as@gmail.com>
2022-04-13 01:05:34 +0200

    Redesign :: Home seen with new struvture

--- app/src/main/java/com/example/universtiyapp/MainActivity.java
+++ app/src/main/java/com/example/universtiyapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.universtiyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES. LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.medB));
+        }
     }
 
     public void start(View v){
