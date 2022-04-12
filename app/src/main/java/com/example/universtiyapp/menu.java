aba2d0d5b4b1a171ad46fe9d18723940e57471fe
ahmed <ahmedmahmoued20211@gmail.com>
2022-04-12 13:41:10 +0200

    m

--- app/src/main/java/com/example/universtiyapp/menu.java
+++ app/src/main/java/com/example/universtiyapp/menu.java
@@ -3,6 +3,7 @@ package com.example.universtiyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +31,7 @@ public class menu extends AppCompatActivity {
 
         menu.checkWebPage checkWebPage = new menu.checkWebPage();
         checkWebPage.execute();
+
 
         newsAlert = findViewById(R.id.newsAlert);
     }
